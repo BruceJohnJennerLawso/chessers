91a00d2064c44a06aa718f0ab2812ff465d7dd47
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2014-09-26 16:43:56 -0400

    pawn legality works

--- public/js/main.js
+++ public/js/main.js
@@ -9,10 +9,57 @@ window.onload = function(){
     game.preload('/public/img/tileset1.png');
     game.preload('/public/img/icon1.png');
     
+    // some game logic required
     game.isPieceSelected = false;
     game.activePiece = null;
+    game.myTurn = false;
+    game.player = 1; // 0 for white, 1 for black, will be null initially - may not be needed
 
     game.locationsUsed = [];
+
+    game.movePiece = function() {
+        // yep.
+    }
+
+    game.checkLegalPawn = function(locPiece, locTile, hasMoved, occupied, pieceID) {
+        // arguments are self explanatory
+
+        if (pieceID[0] === 'w') {
+            // standard movement
+            if ((locTile - locPiece) === -8 && !occupied)
+                return true;
+            // first move can be two spaces forward
+            else if ((locTile - locPiece) === -16 && !occupied && !hasMoved)
+                return true;
+            // attacking
+            else if (((locTile - locPiece) === -7 || (locTile - locPiece) === -9) && occupied)
+                return true;
+        } else {
+            if ((locTile - locPiece) === 8 && !occupied)
+                return true;
+            else if ((locTile - locPiece) === 16 && !occupied && !hasMoved)
+                return true;
+            else if (((locTile - locPiece) === 7 || (locTile - locPiece) === 9) && occupied)
+                return true;
+        }
+
+        return false;
+    }
+
+    game.isLegalMove = function(piece, tile) {
+        // use switch on a piece's type. call appropriate function.
+        pieceID = piece.id;
+        switch(pieceID[1]) {
+
+            case 'p':
+                console.log('hi')
+                return game.checkLegalPawn(piece.loc, tile.loc, piece.hasMoved, tile.occupied, pieceID);
+                break;
+
+            default:
+                return false;
+        }
+    }
 
     
     game.addTile = function(i,j,occupied) {
@@ -21,6 +68,7 @@ window.onload = function(){
         tile.x = 16*i;
         tile.y = 16*j;
         
+        // alternate colours of tiles
         if (j%2 === 0)
             tile.frame = (i%2 === 0) ? 0 : 1;
         else
@@ -32,22 +80,23 @@ window.onload = function(){
         tile.addEventListener(Event.TOUCH_START, function(e) {
 
             if (game.isPieceSelected) {
+                piece = game.activePiece;
 
-                /* TODO:
-                    Check for tile's occupation. If tile is occupied, check 
-                    colour of piece. If it's the user's piece, do nothing,
-                    otherwise check for a kill.
-                */
+                if (game.isLegalMove(piece, this)) {
 
-                var x = Math.floor(e.x/16),
-                    y = Math.floor(e.y/16);
-                // move piece
-                game.activePiece.x = 16*x;
-                game.activePiece.y = 16*y;
-                // update its "location"
-                game.activePiece.loc = x + 8*y;
-                // no piece should be selected anymore
-                game.isPieceSelected = false;
+                    if (piece.hasMoved) piece.hasMoved = true;
+                    var x = Math.floor(e.x/16),
+                        y = Math.floor(e.y/16);
+                    // move piece
+                    piece.x = 16*x;
+                    piece.y = 16*y;
+                    // update location info
+                    game.locationsUsed.pop(game.locationsUsed.indexOf(piece.loc));
+                    piece.loc = x + 8*y;
+                    game.locationsUsed.push(piece.loc);
+                    // no piece should be selected anymore
+                    game.isPieceSelected = false;
+                }
             }
         });
         
@@ -60,8 +109,12 @@ window.onload = function(){
         piece.frame = frame;
         piece.x = x;
         piece.y = y;
-        piece.id = id;
-        piece.loc = Math.floor(x/16) + 8*Math.floor(y/16);
+        piece.id = id; // e.g. bp1 (black pawn 1)
+
+        piece.loc = Math.floor(x/16) + 8*Math.floor(y/16);  // grid number
+        piece.hasMoved = false; // useful for pawns, king, rooks
+        piece.isChessPiece = true;
+        piece.isKinged = false;
         game.locationsUsed.push(piece.loc);
         
         piece.addEventListener(Event.TOUCH_START, function() {
@@ -88,7 +141,7 @@ window.onload = function(){
                 game.addTile(i,j,true);
         }
         
-        game.addPiece(16,0,5,"bp1");
+        game.addPiece(16,16,5,"bp1");
         
     }
     
